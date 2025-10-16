4a0088231bfcfea78ef9caa9d46a339218a57b28
hyperGG32 <196324859+hyperGG32@users.noreply.github.com.>
2025-10-16 21:32:30 +0300

    t5 ok

---
+++ task5.py
@@ -0,0 +1,24 @@
+def is_leap(year):
+    return year % 4 == 0 and year % 100 != 0
+
+def days_in_month(month, year):
+    if is_leap(year):
+        return [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month-1]
+    return [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month-1]
+
+def next_month(month, year):
+    month += 1
+    if month == 13:
+        year += 1
+        month = 1
+    return month, year
+
+mn = int(input("Input month as int pls: "))
+yr = int(input("Input year as int pls: "))
+
+lsofmonthsintext = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+for i in range(3):
+    print(lsofmonthsintext[mn-1], yr,'-', days_in_month(mn, yr), "days")
+    mn, yr = next_month(mn, yr)
+print(f"(Leap year): {"Yes" if is_leap(yr) else "No"}")
