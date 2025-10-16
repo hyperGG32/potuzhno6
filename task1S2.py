1eea19c6358562b8646507697d3bb01259ce9caf
hyperGG32 <196324859+hyperGG32@users.noreply.github.com.>
2025-10-16 20:12:43 +0300

    t1 superpro

--- task1S2.py
+++ task1S2.py
@@ -4,9 +4,14 @@ def c_to_f(celsius):
 def f_to_c(fahrenheit):
     return 5/9*(fahrenheit-32)
 
-t_in_c = [-10, -5, 0, 12.5, 23.1, 35, 41, 100, 250, 300, 420]
+temperatures = [-10, -5, 0, 12.5, 23.1, 35, 41, 100, 250, 300, 420]
 
 print("Celsius | Fahrenheit")
 print("------- | -----------")
-for t in t_in_c:
+for t in temperatures:
     print(f"{t:<7} | {c_to_f(t):<10}")
+
+print("Fahrenheit | Celsius")
+print("---------- | --------")
+for t in temperatures:
+    print(f"{t:<10} | {f_to_c(t):<7}")
