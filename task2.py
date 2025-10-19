1d1602e46b3351ee752f910c5937838746ac4082
hyperGG32 <196324859+hyperGG32@users.noreply.github.com.>
2025-10-19 21:08:00 +0300

    t5: made functions prettier and a bit of optimisations t2: made functions prettier

--- task2.py
+++ task2.py
@@ -1,28 +1,32 @@
-nums = [int(num) for num in input("Input nums in spaces pls: ").split() if num.isnumeric()]
+nums = [int(num) for num in input("Input nums in spaces pls: ").split() if num.isdecimal()]
 # appending only valid ones (i think that works)
 
 if not nums:
     print("Error!")
     exit(1)
 
-def get_min(ls):
+def get_min(ls: list[int | float]) -> int | float:
+    """Returns the smallest element in the list"""
     mini = ls[0]
     for n in ls:
         if mini > n:
             mini = n
     return mini
 
-def get_max(ls):
+def get_max(ls: list[int | float]) -> int | float:
+    """Returns the biggest element in the list"""
     maxi = ls[0]
     for n in ls:
         if maxi < n:
             maxi = n
     return maxi
 
-def get_average(ls):
+def get_average(ls: list[int | float]) -> float:
+    """Returns average of all elements in the list"""
     return sum(ls)/len(ls)
 
-def get_median(ls):
+def get_median(ls: list[int | float]) -> float | int:
+    """Returns the mid-element in the list"""
     ls.sort()
 
     if len(ls) % 2 == 0:
--- task5.py
+++ task5.py
@@ -1,12 +1,16 @@
-def is_leap(year):
+def is_leap(year: int) -> bool:
+    """Returns whether the year is leap or not"""
     return year % 4 == 0 and year % 100 != 0
 
-def days_in_month(month, year):
+def days_in_month(month: int, year: int) -> int:
+    """Returns count of days in month"""
     if is_leap(year):
-        return [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month-1]
-    return [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month-1]
+        return (31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)[month-1]
+    return (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)[month-1]
 
-def next_month(month, year):
+def next_month(month: int, year: int) -> tuple[int, int]:
+    """Switch to next month, automaticly changes year
+    Returns a tuple (month, year)"""
     month += 1
     if month == 13:
         year += 1
@@ -16,7 +20,7 @@ def next_month(month, year):
 mn = int(input("Input month as int pls: "))
 yr = int(input("Input year as int pls: "))
 
-lsofmonthsintext = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+lsofmonthsintext = ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
 
 for i in range(3):
     print(lsofmonthsintext[mn-1], yr,'-', days_in_month(mn, yr), "days")
